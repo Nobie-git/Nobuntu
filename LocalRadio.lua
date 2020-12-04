ff5dcdd1b650f055c1c01e30a6ee7ee3b4b64045
Nobie <63230056+Nobie-git@users.noreply.github.com>
2020-12-04 15:15:56 +0200

    Update LocalRadio.lua

--- LocalRadio.lua
+++ LocalRadio.lua
@@ -192,7 +192,7 @@ UserInputService.InputChanged:Connect(function(input)
 end)
 local sound = Instance.new("Sound",game.SoundService)
 Input.FocusLost:Connect(function()
-	sound.SoundId = Input.Text	
+	sound.SoundId = "rbxassetid://" .. Input.Text	
 end)
 local PlayDB = false
 Play.MouseButton1Click:Connect(function()
